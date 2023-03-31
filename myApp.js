ab96791be4fcffb24b31e5d72187dac56f77ba19
BrianC <brian@debian-BULLSEYE-live-builder-AMD64>
2023-03-30 22:12:13 -0500

    removed send argument on final handler function

--- myApp.js
+++ myApp.js
@@ -42,7 +42,7 @@ app.get("/json", function(req, res){
 app.get("/now", function(req,res,next){
     req.time = new Date().toString();
     next();
-}, function(req,res,next){
+}, function(req,res){
     res.send({time: req.time});
 })
 
